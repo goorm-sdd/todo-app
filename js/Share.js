d32cf02409e5a93390d7d686fbc88eadb60c6005
cherish0-0 <jiyu01160@naver.com>
2025-04-16 15:47:04 +0900

    feat: 공유 기능 완성

---
+++ js/Share.js
@@ -0,0 +1,12 @@
+import {todos} from "../script.js";
+
+//배열 문자열화
+const textList = todos.map((item, id) => `${id + 1}. ${item.text}`).join('\n');
+
+
+//item 클릭 시 복사 및 완료 알림
+    document.getElementById("share-btn").addEventListener("click", () => {
+        navigator.clipboard.writeText(textList).then(() => {
+            alert("할 일 목록이 복사되었습니다!");
+        })
+    });
