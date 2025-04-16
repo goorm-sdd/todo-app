70c528779a2f42880ea9b435668b2e78d0e62a83
cherish0-0 <jiyu01160@naver.com>
2025-04-16 17:33:07 +0900

    fix: 공유 기능 수정 textList가 한 번만 불러와져서 새로고침을 하지 않으면 수정 내용 반영 안됨 -> 공유버튼 클릭할 때마다 textList 받아오도록 수정,
    배열 길이 체크 잘못함
    -> textList를 비교하지 않고 todos의 길이를 체크하여 해당 부분 수정

--- js/Share.js
+++ js/Share.js
@@ -1,20 +1,17 @@
 import {todos} from "../script.js";
 
-//배열 text만 가져오기
-const textList = todos.filter(item => item.text.trim() !== "")
-.map((item, id) => `${id + 1}. ${item.text}`)
-.join('\n');
-
-
 //share-btn 클릭 시 복사 및 완료 알림 (todos가 비어있을 시 복사하지 않음)
-if (todos.length > 0) {
-    document.getElementById("share-btn").addEventListener("click", () => {
-        navigator.clipboard.writeText(textList).then(() => {
+
+document.getElementById("share-btn").addEventListener("click", () => {
+    const textList = todos.filter(item => item.text.trim() !== "")
+        .map((item, id) => `${id + 1}. ${item.text}`);
+    
+    if (textList.length > 0) {
+        const joinedText = textList.join('\n');
+        navigator.clipboard.writeText(joinedText).then(() => {
             alert("할 일 목록이 복사되었습니다!");
         })
-    });
-} else {
-    document.getElementById("share-btn").addEventListener("click", () => {
+    } else {
         alert("복사할 할 일 목록이 없습니다.");
-    });
-}
+    }
+})
