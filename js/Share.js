e2deee3dfee677d891f321c48e1966630acfb47a
cherish0-0 <jiyu01160@naver.com>
2025-04-16 15:58:56 +0900

    chore: 주석 오타 수정

--- js/Share.js
+++ js/Share.js
@@ -4,7 +4,7 @@ import {todos} from "../script.js";
 const textList = todos.map((item, id) => `${id + 1}. ${item.text}`).join('\n');
 
 
-//item 클릭 시 복사 및 완료 알림
+//share-btn 클릭 시 복사 및 완료 알림
     document.getElementById("share-btn").addEventListener("click", () => {
         navigator.clipboard.writeText(textList).then(() => {
             alert("할 일 목록이 복사되었습니다!");
